b8c44413f0d537f61ce73a81597805bbc5182f24
codeelevate321 <500098457@stu.upes.ac.in>
2022-10-14 10:07:31 +0530

    done the first type of hashmap

--- src/HashMap/Hashmap.java
+++ src/HashMap/Hashmap.java
@@ -1,5 +1,8 @@
 import java.net.InterfaceAddress;
 import java.util.*;
+
+
+
 public class Hashmap {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -13,21 +16,33 @@ public class Hashmap {
 
         thing.put("object2",500984221);
         thing.put("object3",5009954);
-        System.out.println(thing);
+       System.out.println(thing);
         System.out.println("size of this is "+thing.size());
         //gives the size indexing is 1,2,3.......
 
         //to check if the key is present or not we use  "containsKey" function
         if(thing.containsKey("name")){
             Integer value=thing.get("name");
-            System.out.println("the value of the seached key is "+value);
-        }System.out.println("please enter the key to be searched  ");
-        String s=sc.nextline();
+            System.out.println("the value of the searched key is "+value);
+        }
+
+        //let's search from the user this time
+        System.out.println("please enter the key to be searched  ");
+        String s=sc.nextLine();
         if(thing.containsKey(s));{
         Integer a=thing.get("name");
         System.out.println("the value of the seached key is "+a);}
 
             thing.get(s);
+        }
+//follows:
+//
+// 1   HashMap()    =>done =>Syntax is"" HashMap<K, V> hm = new HashMap<K, V>();""
+//  2  HashMap(int initialCapacity)
+//    3 HashMap(int initialCapacity, float loadFactor)
+//      4HashMap(Map map)
+
+
 
 
 
@@ -36,4 +51,3 @@ public class Hashmap {
 
 
     }
-}
